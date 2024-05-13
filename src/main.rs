988c5c2cf5672c27fd62ee6d7d00effdf7967b45
ge9 <kensho.tsurusaki@gmail.com>
2024-05-14 02:58:54 +0900

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::broadcast;
 
+use tokio::net::UdpSocket;
+use std::sync::Arc;
+use std::collections::HashMap;
+use tokio::process::Command;
+
 type BoxedError = Box<dyn std::error::Error + Sync + Send + 'static>;
 static DEBUG: AtomicBool = AtomicBool::new(false);
 const BUF_SIZE: usize = 1024;
@@ -14,7 +19,7 @@ fn print_usage(program: &str, opts: Options) {
     let program_path = std::path::PathBuf::from(program);
     let program_name = program_path.file_stem().unwrap().to_string_lossy();
     let brief = format!(
-        "Usage: {} REMOTE_HOST:PORT [-b BIND_ADDR] [-l LOCAL_PORT]",
+        "Usage: {} REMOTE_HOST:PORT [-b BIND_ADDR] [-l LOCAL_PORT] [-h SSH_HOST] [-c COMMAND] ...",
         program_name
     );
     print!("{}", opts.usage(&brief));
@@ -37,6 +42,18 @@ async fn main() -> Result<(), BoxedError> {
         "local-port",
         "The local port to which tcpproxy should bind to, randomly chosen otherwise",
         "LOCAL_PORT",
+    );
+    opts.reqopt(
+        "h",
+        "host",
+        "The ssh host to connect to, configured in .ssh/config",
+        "SSH_HOST",
+    );
+    opts.optmulti(
+        "c",
+        "command",
+        "The command to call remote helper. Use this option multiple time for complex commands.",
+        "COMMAND",
     );
     opts.optflag("d", "debug", "Enable debug mode");
 
@@ -68,11 +85,50 @@ async fn main() -> Result<(), BoxedError> {
         Some(addr) => addr,
         None => "127.0.0.1".to_owned(),
     };
-
-    forward(&bind_addr, local_port, remote).await
+    let ssh_host = matches.opt_str("h").unwrap();
+    let ssh_command = matches.opt_strs("c");
+    forward(&bind_addr, local_port, remote, ssh_host, ssh_command).await
 }
 
-async fn forward(bind_ip: &str, local_port: i32, remote: String) -> Result<(), BoxedError> {
+async fn read_from_stdout(
+    stdout_reader0: Arc<tokio::sync::Mutex<tokio::process::ChildStdout>>,
+    conmap: Arc<tokio::sync::Mutex<HashMap<(u8, u8, u8, u8, u8, u8), Arc<UdpSocket>>>>,
+) {
+    let mut stdout = stdout_reader0.lock().await;
+    let mut buffer = [0; 8];
+    loop {
+        stdout.read_exact(&mut buffer).await.expect("error reading header");
+
+        let length = u16::from_be_bytes([buffer[6], buffer[7]]);
+        let mut data = vec![0; length as usize];
+        stdout.read_exact(&mut data).await.expect("error reading data");
+        if let Some(u) = conmap.lock().await.get(&(
+            buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5],
+        )) {
+            u.send(&data).await.unwrap();
+        }
+    }
+}
+
+async fn forward(bind_ip: &str, local_port: i32, remote: String, ssh_host: String, ssh_command: Vec<String>) -> Result<(), BoxedError> {
+    let mut child = Command::new("ssh")
+        .arg(ssh_host).args(ssh_command.as_slice())
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .spawn()
+        .expect("Failed to start command");
+    let stdin = child.stdin.take().expect("Failed to open stdin");
+    let stdout = child.stdout.take().expect("Failed to open stdout");
+    let stdin_writer = Arc::new(tokio::sync::Mutex::new(stdin));
+    let stdout_reader = Arc::new(tokio::sync::Mutex::new(stdout));
+    let conmap = HashMap::new();
+    let conmap0 = Arc::new(tokio::sync::Mutex::new(conmap));
+    let conmap2 = Arc::clone(&conmap0);
+    let stdout_reader2 = Arc::clone(&stdout_reader);
+   
+    tokio::spawn(async move {
+        read_from_stdout( stdout_reader2, conmap2).await
+    });
     // Listen on the specified IP and port
     let bind_addr = if !bind_ip.starts_with('[') && bind_ip.contains(':') {
         // Correctly format for IPv6 usage
@@ -103,6 +159,10 @@ async fn forward(bind_ip: &str, local_port: i32, remote: String) -> Result<(), B
         read: &mut R,
         write: &mut W,
         mut abort: broadcast::Receiver<()>,
+        r2c: bool, //remote to local
+        state: Arc<std::sync::Mutex<bool>>,
+        stdin_writer: Arc<tokio::sync::Mutex<tokio::process::ChildStdin>>,
+        conmap: Arc<tokio::sync::Mutex<HashMap<(u8, u8, u8, u8, u8, u8), Arc<UdpSocket>>>>, //UdpSocket doesn't require Mutex
     ) -> tokio::io::Result<usize>
     where
         R: tokio::io::AsyncRead + Unpin,
@@ -110,6 +170,7 @@ async fn forward(bind_ip: &str, local_port: i32, remote: String) -> Result<(), B
     {
         let mut copied = 0;
         let mut buf = [0u8; BUF_SIZE];
+        let mut sv_tuple = (0,0,0,0,0,0);
         loop {
             let bytes_read;
             tokio::select! {
@@ -132,17 +193,71 @@ async fn forward(bind_ip: &str, local_port: i32, remote: String) -> Result<(), B
                 break;
             }
 
+            if *state.lock().unwrap() {
+                sv_tuple = (buf[4], buf[5], buf[6], buf[7], buf[8], buf[9]);
+                let stdin_writer1 = Arc::clone(&stdin_writer);
+                tokio::spawn(async move {
+                    let mut stdin = stdin_writer1.lock().await;
+                    stdin.write_all(&[buf[4], buf[5], buf[6], buf[7], 0, 0, buf[8], buf[9], 0]).await.unwrap();
+                    stdin.flush().await.unwrap();
+                });
+                let stdin_writer2 = Arc::clone(&stdin_writer);
+                let new_socket = tokio::task::block_in_place(|| async {
+                    let udp_socket = UdpSocket::bind("127.0.0.1:0").await.unwrap();
+                    let udp_port = udp_socket.local_addr().unwrap().port();
+                    let mutsocket = Arc::new(udp_socket);
+                    eprintln!("[client-info] Assigned {} for {:?}", udp_port, sv_tuple);
+                    conmap.lock().await.insert(sv_tuple, Arc::clone(&mutsocket));
+                    tokio::spawn(async move {
+                        let mut buf = [0; 1555];
+                        {
+                            let (size, src) = mutsocket.recv_from(&mut buf).await.expect("error in receiving first packet");
+                            mutsocket.connect(src).await.unwrap();
+                            let s = u16::to_be_bytes(size as u16);
+                            eprintln!("[client-info] Received the first UDP packet from {}, connected", src);
+                            let stdin_writer = Arc::clone(&stdin_writer2);
+                            let mut stdin = stdin_writer.lock().await;
+                            stdin.write_all(&[sv_tuple.0, sv_tuple.1, sv_tuple.2, sv_tuple.3,sv_tuple.4, sv_tuple.5, s[0], s[1]]).await.unwrap();
+                            stdin.write_all(&buf[..size]).await.unwrap();
+                            stdin.flush().await.unwrap();
+                        }
+                        loop {
+                            let (size, _) = mutsocket.recv_from(&mut buf).await.expect("error in receiving packet");
+                            let s = u16::to_be_bytes(size as u16);
+                            let stdin_writer = Arc::clone(&stdin_writer2);
+                            let mut stdin = stdin_writer.lock().await;
+                            stdin.write_all(&[sv_tuple.0, sv_tuple.1, sv_tuple.2, sv_tuple.3,sv_tuple.4, sv_tuple.5, s[0], s[1]]).await.unwrap();
+                            stdin.write_all( &buf[..size]).await.unwrap();
+                            stdin.flush().await.unwrap();
+                        }
+                    });
+                    return udp_port;
+                }).await;
+                let a = new_socket.to_be_bytes();
+                buf[4] = 127;buf[5] = 0;buf[6] = 0;buf[7] = 1;buf[8] = a[0];buf[9] = a[1]
+            }
+            {
+                let mut s = state.lock().unwrap();
+                *s =  !r2c && buf[0] == 5 && buf[1] == 3;
+            }
             // While we ignore some read errors above, any error writing data we've already read to
             // the other side is always treated as exceptional.
             write.write_all(&buf[0..bytes_read]).await?;
             copied += bytes_read;
         }
 
+        if r2c {//or !r2c (either will be ok)
+            let mut stdin = stdin_writer.lock().await;
+            stdin.write_all(&[sv_tuple.0, sv_tuple.1, sv_tuple.2, sv_tuple.3, 0, 0, sv_tuple.4, sv_tuple.5, 1]).await.unwrap();
+            stdin.flush().await.unwrap();
+        }
         Ok(copied)
     }
 
     loop {
         let (mut client, client_addr) = listener.accept().await?;
+        let stdin_writer0 = Arc::clone(&stdin_writer);
+        let conmap1 = Arc::clone(&conmap0);
 
         tokio::spawn(async move {
             println!("New connection from {}", client_addr);
@@ -158,11 +273,12 @@ async fn forward(bind_ip: &str, local_port: i32, remote: String) -> Result<(), B
             let (mut client_read, mut client_write) = client.split();
             let (mut remote_read, mut remote_write) = remote.split();
 
+            let r2c = Arc::new(std::sync::Mutex::new(false));
             let (cancel, _) = broadcast::channel::<()>(1);
             let (remote_copied, client_copied) = tokio::join! {
-                copy_with_abort(&mut remote_read, &mut client_write, cancel.subscribe())
+                copy_with_abort(&mut remote_read, &mut client_write, cancel.subscribe(),true, Arc::clone(&r2c), Arc::clone(&stdin_writer0), Arc::clone(&conmap1))
                     .then(|r| { let _ = cancel.send(()); async { r } }),
-                copy_with_abort(&mut client_read, &mut remote_write, cancel.subscribe())
+                copy_with_abort(&mut client_read, &mut remote_write, cancel.subscribe(),false, r2c            , Arc::clone(&stdin_writer0), Arc::clone(&conmap1))
                     .then(|r| { let _ = cancel.send(()); async { r } }),
             };
 
